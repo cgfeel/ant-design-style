02f4c05f6fa8d1d0609384696d0b1ed0e195d1bc
levi <cgfeel@gmail.com>
2024-02-07 16:48:02 +0800

    init

---
+++ src/Router.tsx
@@ -0,0 +1,23 @@
+import { Suspense, lazy } from 'react';
+
+const router = {
+    '/': lazy(() => import('./App')),
+};
+
+type RoterType = typeof router;
+type RouterKey =  keyof RoterType;
+
+export default function Router() {
+    const { pathname } = window.location;
+    const Router = Object.keys(router).indexOf(pathname) < 0 ? undefined : router[pathname as RouterKey];
+
+    return Router === undefined ? (
+        <>This page is 404.</>
+    ) : (
+        <Suspense
+            fallback={<>Loading...</>}
+        >
+            <Router />
+        </Suspense>
+    );
+}
